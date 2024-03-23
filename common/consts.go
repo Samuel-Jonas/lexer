3c5107cf7a834cc2fd59f5d6565c223ba181f5cc
Samuel-Jonas <samueljonas@alu.ufc.br>
2024-03-23 16:23:20 -0300

    end the constants to keywords

--- common/consts.go
+++ common/consts.go
@@ -9,8 +9,8 @@ const (
 	INT            = "INT"     // integer data type
 	FLOAT          = "FLOAT"   // float data ype
 	STRING         = "STRING"  // string data type
-	IDENTIFIER     = "ID"      // identifier or variable
-	KEYWORD        = "KEYWORD" // reserved word from the language
+	IDENTIFIER     = "ID"      // identifier or variable type
+	KEYWORD        = "KEYWORD" // reserved word type
 	PLUS           = "PLUS"    // (+) addition operation symbol
 	MINUS          = "MINUS"   // (-) subtraction operation symbol
 	TIMES          = "TIMES"   // (*) multiplication operation symbol
@@ -31,4 +31,14 @@ const (
 	ARROW          = "ARROW"   // (->) arrow symbol
 	NEWLINE        = "NEWLINE" // (/n) new line
 	EOF            = "EOF"     // end of file
+	VAR            = "var"     // keyword var
+	AND            = "and"     // keyword and
+	OR             = "or"      // keyword or
+	NOT            = "not"     // keyword not
+	WHILE          = "while"   // keyword while
+	DO             = "do"      // keyword do
+	END            = "end"     // keyword end
+	FALSE          = "false"   // keyword false
+	TRUE           = "true"    // keyword true
+	NULL           = "null"    // keyword null
 )
