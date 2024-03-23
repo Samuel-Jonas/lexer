bc4812f2fa4cfe631b71c05615aae65a25f18436
Samuel-Jonas <samueljonas@alu.ufc.br>
2024-03-23 15:51:51 -0300

    some constants

--- common/consts.go
+++ common/consts.go
@@ -1,1 +1,34 @@
 package common
+
+const (
+	LETTERS_LOWER  = "abcdefghiklmnopqrstuvwxyz"
+	LETTERS_UPPER  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	DIGITS         = "0123456789"
+	LETTERS        = LETTERS_LOWER + LETTERS_UPPER
+	LETTERS_DIGITS = DIGITS + LETTERS
+	INT            = "INT"     // integer data type
+	FLOAT          = "FLOAT"   // float data ype
+	STRING         = "STRING"  // string data type
+	IDENTIFIER     = "ID"      // identifier or variable
+	KEYWORD        = "KEYWORD" // reserved word from the language
+	PLUS           = "PLUS"    // (+) addition operation symbol
+	MINUS          = "MINUS"   // (-) subtraction operation symbol
+	TIMES          = "TIMES"   // (*) multiplication operation symbol
+	DIV            = "DIV"     // (/) division operation symbol
+	POW            = "POW"     // (^) exponentiation opeation symbol
+	EQ             = "EQ"      // (==) equality comparison symbol
+	LPAR           = "LPAR"    // ( left parenthesis
+	RPAR           = "RPAR"    // ) right parenthesis
+	LSQUARE        = "LSQUARE" // ([) left square bracket
+	RSQUARE        = "RSQUARE" // (]) right square bracket
+	EQUAL          = "EQUAL"   // (==) equal comparison symbol
+	NE             = "NE"      // (!=) not equal comparison symbol
+	LT             = "LT"      // (<) less then comparison symbol
+	GT             = "GT"      // (>) greater then comparison symbol
+	LTE            = "LTE"     // (<=) less then or equal to comparison symbol
+	GTE            = "GTE"     // (>=) greater then or equal to comparison symbol
+	COMMA          = "COMMA"   // (,) comma symbol
+	ARROW          = "ARROW"   // (->) arrow symbol
+	NEWLINE        = "NEWLINE" // (/n) new line
+	EOF            = "EOF"     // end of file
+)
